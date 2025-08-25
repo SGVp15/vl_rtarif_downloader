ffd5cc38e4fa9dea110dce0651a5049a226f5427
SGVp15 <phosphorusp15@mail.ru>
2025-08-25 12:29:54 +0300

    main

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 TARGET_URLS = {
-    'https://files.daobit.ru/RATLAS/RENEW/': 'downloads/ratlas',
-    'https://files.daobit.ru/RINFO/RENEW/': 'downloads/rinfo',
-    'https://files.daobit.ru/RTARIFF/RENEW/': 'downloads/rtariff',
+    'https://files.daobit.ru/RATLAS/RENEW/': '\\\\fs/data/downloads/ratlas',
+    'https://files.daobit.ru/RINFO/RENEW/': '\\\\fs/data/downloads/rinfo',
+    'https://files.daobit.ru/RTARIFF/RENEW/': '\\\\fs/data/downloads/rtariff',
 }
 
 NUM_LAST_FILES_TO_DOWNLOAD = 5
